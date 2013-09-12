06d7cfcbc6a5147ae78a8f33cb5a00c6ce68cfe4
Fred <fred.rodrigo@gmail.com>
2013-09-12 21:11:54 +0200

    Tag label is required

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,5 +3,5 @@ class Tag < ActiveRecord::Base
   has_and_belongs_to_many :destinations
   has_and_belongs_to_many :plannings
 
-#  validates :customer, presence: true
+  validates :label, presence: true
 end
