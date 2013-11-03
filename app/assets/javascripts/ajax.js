fa82ab681c4c6a9ef65ee8e4c626a3fb89fca433
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2013-11-03 12:29:36 +0100

    Catch ajax error

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -13,3 +13,14 @@ function completeWaiting() {
     $('body').removeClass('waiting');
   }
 }
+
+function ajaxError(request, status, error) {
+console.log(request);
+console.log(status);
+console.log(error);
+  $(".main").prepend(
+    '<div class="alert fade in alert-error">' +
+    '<button class="close" data-dismiss="alert">×</button>' +
+    status + ' ' + request.responseText +
+   '</div>');
+}
