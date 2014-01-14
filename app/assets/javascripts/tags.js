cca46a9ef7498feb147e47b471e191195753ead8
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-01-14 18:22:48 +0100

    Add license header

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,2 +1,20 @@
+// Copyright © Mapotempo, 2013-2014
+//
+// This file is part of Mapotempo.
+//
+// Mapotempo is free software. You can redistribute it and/or
+// modify since you respect the terms of the GNU Affero General
+// Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// Mapotempo is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the Licenses for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Mapotempo. If not, see:
+// <http://www.gnu.org/licenses/agpl.html>
+//
+
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
