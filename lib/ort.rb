f071a558bef36994529513d7450fb83d1586cbd6
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-02-06 12:02:14 +0100

    Use matrix hash as optimizer cache key

--- lib/ort.rb
+++ lib/ort.rb
@@ -23,11 +23,12 @@ module Ort
   @url = Mapotempo::Application.config.optimize_url
 
   def self.optimize(capacity, matrix, time_window)
-    key = {capacity: capacity, matrix: matrix, time_window: time_window}.to_json
+    key = {capacity: capacity, matrix: matrix.hash, time_window: time_window}.to_json
 
     result = @cache.read(key)
     if !result
-      result = RestClient.post @url, data: key, content_type: :json, accept: :json
+      data = {capacity: capacity, matrix: matrix, time_window: time_window}.to_json
+      result = RestClient.post @url, data: data, content_type: :json, accept: :json
       @cache.write(key, result)
     end
 
