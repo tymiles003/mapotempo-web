3a030b3073ac44657afb9e416bb58c802bb44909
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-07-03 13:10:51 +0200

    Fix link_back when link new and edit step

--- app/controllers/concerns/link_back.rb
+++ app/controllers/concerns/link_back.rb
@@ -1,17 +1,21 @@
+require 'cgi'
+
 module LinkBack
   extend ActiveSupport::Concern
 
   included do
-    after_filter "save_link_back", only: [:new, :edit]
+    after_filter 'save_link_back', only: [:new, :edit]
   end
 
   def save_link_back
     # session[:previous_url] is a Rails built-in variable to save last url.
     if request.format == Mime::HTML
-      if params["back"]
-        session[:link_back] = URI(request.referer).path
-      else
-      session.delete(:link_back)
+      referer_uri = request.referer ? URI.parse(request.referer) : nil
+      referer_params = referer_uri && referer_uri.query ? CGI::parse(referer_uri.query) : nil
+      if referer_uri && params['back']
+        session[:link_back] = referer_uri.path
+      elsif !(referer_uri && referer_params && referer_params['back'])
+        session.delete(:link_back)
       end
     end
   end
