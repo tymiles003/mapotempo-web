5cf2dbbf09ab36367ce016f05a002b26e56328e2
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-07-10 16:46:41 +0200

    Add planning model tests

--- test/models/planning_test.rb
+++ test/models/planning_test.rb
@@ -35,4 +35,52 @@ class PlanningTest < ActiveSupport::TestCase
       o.set_destinations([[destinations(:destination_one)]] * (o.routes.size+1))
     }
   end
+
+  test "should vehicle_add" do
+    o = plannings(:planning_one)
+    assert_difference('Stop.count', 2) do
+      assert_difference('Route.count', 1) do
+        o.vehicle_add(vehicles(:vehicle_two))
+      end
+    end
+  end
+
+  test "should vehicle_remove" do
+    o = plannings(:planning_one)
+    assert_difference('Stop.count', -4) do
+      assert_difference('Route.count', -1) do
+        o.vehicle_remove(vehicles(:vehicle_one))
+      end
+    end
+  end
+
+  test "should destination_add" do
+    o = plannings(:planning_one)
+    assert_difference('Stop.count') do
+      o.destination_add(destinations(:destination_two))
+    end
+  end
+
+  test "should destination_remove" do
+    o = plannings(:planning_one)
+    assert_difference('Stop.count', -1) do
+      o.destination_remove(destinations(:destination_one))
+    end
+  end
+
+  test "should compute" do
+    o = plannings(:planning_one)
+    o.compute
+    o.routes.select{ |r| r.vehicle }.each{ |r|
+      assert_not  r.out_of_date
+    }
+  end
+
+  test "should out_of_date" do
+    o = plannings(:planning_one)
+    assert_not o.out_of_date
+
+    o.routes[1].out_of_date = true
+    assert o.out_of_date
+  end
 end
