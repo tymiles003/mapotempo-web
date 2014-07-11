1e6343cc02041be74385e2b6f6caa98c9ba2f26c
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-07-11 15:33:35 +0200

    link back, make concern content private

--- app/controllers/concerns/link_back.rb
+++ app/controllers/concerns/link_back.rb
@@ -7,20 +7,21 @@ module LinkBack
     after_filter 'save_link_back', only: [:new, :edit]
   end
 
-  def save_link_back
-    # session[:previous_url] is a Rails built-in variable to save last url.
-    if request.format == Mime::HTML
-      referer_uri = request.referer ? URI.parse(request.referer) : nil
-      referer_params = referer_uri && referer_uri.query ? CGI::parse(referer_uri.query) : nil
-      if referer_uri && params['back']
-        session[:link_back] = referer_uri.path
-      elsif !(referer_uri && referer_params && referer_params['back'])
-        session.delete(:link_back)
+  private
+    def save_link_back
+      # session[:previous_url] is a Rails built-in variable to save last url.
+      if request.format == Mime::HTML
+        referer_uri = request.referer ? URI.parse(request.referer) : nil
+        referer_params = referer_uri && referer_uri.query ? CGI::parse(referer_uri.query) : nil
+        if referer_uri && params['back']
+          session[:link_back] = referer_uri.path
+        elsif !(referer_uri && referer_params && referer_params['back'])
+          session.delete(:link_back)
+        end
       end
     end
-  end
 
-  def link_back
-    session.delete(:link_back)
-  end
+    def link_back
+      session.delete(:link_back)
+    end
 end
