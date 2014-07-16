c4e898969b998d9a66758a72f91b66d280cfb0ed
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-07-16 09:30:44 +0200

    Add job import test

--- test/jobs/importer_test.rb
+++ test/jobs/importer_test.rb
@@ -11,6 +11,28 @@ class ImporterTest < ActionController::TestCase
     end
   end
 
+  test "shoud import many-utf-8" do
+    Planning.all.each(&:destroy)
+    @customer.destinations.destroy_all
+    assert_difference('Destination.count', 6) do
+      Importer.import(false, @customer, "test/fixtures/files/import_many-utf-8.csv", "text")
+    end
+    o = Destination.find{|d| d.customer_id}
+    assert_equal "Point 1", o.name
+    assert_equal ["Nantes"], o.tags.collect(&:label)
+  end
+
+  test "shoud import many-iso" do
+    Planning.all.each(&:destroy)
+    @customer.destinations.destroy_all
+    assert_difference('Destination.count', 6) do
+      Importer.import(false, @customer, "test/fixtures/files/import_many-iso.csv", "text")
+    end
+    o = Destination.find{|d| d.customer_id}
+    assert_equal "Point 1", o.name
+    assert_equal ["Nantes"], o.tags.collect(&:label)
+  end
+
   test "shoud not import" do
     assert_difference('Destination.count', 0) do
       assert_raise RuntimeError do
