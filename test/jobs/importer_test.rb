4bbeb1b3e4abbdbe630022a273ec8f2b50a9f71b
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-07-18 18:38:12 +0200

    Fix import test

--- test/jobs/importer_test.rb
+++ test/jobs/importer_test.rb
@@ -6,15 +6,19 @@ class ImporterTest < ActionController::TestCase
   end
 
   test "shoud import" do
-    assert_difference('Destination.count', 1) do
-      Importer.import(false, @customer, "test/fixtures/files/import_one.csv", "text")
+    assert_difference('Planning.count') do
+      assert_difference('Destination.count') do
+        assert_difference('Stop.count', 1 + 3 + 3) do
+          Importer.import(false, @customer, "test/fixtures/files/import_one.csv", "text")
+        end
+      end
     end
   end
 
   test "shoud import many-utf-8" do
     Planning.all.each(&:destroy)
     @customer.destinations.destroy_all
-    assert_difference('Destination.count', 6) do
+    assert_difference('Destination.count', 5) do
       Importer.import(false, @customer, "test/fixtures/files/import_many-utf-8.csv", "text")
     end
     o = Destination.find{|d| d.customer_id}
