7f27657adb75b0c166c5cd50d353fdc5a08528c9
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-09-18 15:28:20 +0200

    Plannings API Spec

---
+++ spec/api/v01/plannings_spec.rb
@@ -0,0 +1,114 @@
+require 'rails_helper'
+
+describe V01::Plannings do
+  fixtures :all
+
+  before(:each) do
+    @planning = plannings(:planning_one)
+  end
+
+  def api(part = nil)
+    part = part ? '/' + part.to_s : ''
+    "/api/0.1/plannings#{part}.json?api_key=testkey1"
+  end
+
+  describe :list do
+    it 'Return customer''s plannings' do
+      get api()
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body).size).to eq @planning.customer.plannings.size
+    end
+  end
+
+  describe :get do
+    it 'Return a planning' do
+      get api(@planning.id)
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body)['name']).to eq @planning.name
+    end
+  end
+
+  describe :create do
+    it 'Create a planning' do
+      expect{
+        @planning.name = 'new name'
+        post api(), @planning.attributes
+        expect(response.status).to eq(201)
+      }.to change{Planning.count}.by(1)
+    end
+  end
+
+  describe :update do
+    it 'Update a planning' do
+      @planning.name = 'new name'
+      put api(@planning.id), @planning.attributes
+      expect(response.status).to eq(200)
+    end
+
+    it 'Return a planning' do
+      get api(@planning.id)
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body)['name']).to eq @planning.name
+    end
+  end
+
+  describe :destroy do
+    it 'Destroy a planning' do
+      expect{
+        delete api(@planning.id)
+        expect(response.status).to eq(200)
+      }.to change{Planning.count}.by(-1)
+    end
+  end
+
+#  describe :move do
+#    it 'Move a stop between routes' do
+#      patch api("#{@planning.id}/move")
+#      expect(response.status).to eq(200)
+#    end
+#  end
+
+  describe :refresh do
+    it 'Force recompute the planning after parameter update' do
+      get api("#{@planning.id}/refresh")
+      expect(response.status).to eq(200)
+    end
+  end
+
+#  describe :switch do
+#    it 'Switch two vehicles' do
+#      patch api("#{@planning.id}/switch")
+#      expect(response.status).to eq(200)
+#    end
+#  end
+
+#  describe :automatic_insert do
+#    it 'Suggest a place for an unaffected destination' do
+#      patch api("#{@planning.id}/automatic_insert")
+#      expect(response.status).to eq(200)
+#    end
+#  end
+
+#  describe :update_stop do
+#    it 'Set stop status' do
+#      patch api("#{@planning.id}/update_stop")
+#      expect(response.status).to eq(200)
+#    end
+#  end
+
+#  describe :optimize_route do
+#    it 'Starts asynchronous route optimization' do
+#      get api("#{@planning.id}/optimize_route")
+#      expect(response.status).to eq(200)
+#    end
+#  end
+
+  describe :duplicate do
+    it 'Clone the planning' do
+      expect{
+        patch api("#{@planning.id}/duplicate")
+        expect(response.status).to eq(200)
+      }.to change{Planning.count}.by(1)
+    end
+  end
+end
