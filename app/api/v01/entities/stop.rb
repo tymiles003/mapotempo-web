1beed92d1236938158cd4b8ea7f17b708e2909fb
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-09-26 15:02:11 +0200

    Fix error on null time into stop api

--- app/api/v01/entities/stop.rb
+++ app/api/v01/entities/stop.rb
@@ -4,7 +4,7 @@ class V01::Entities::Stop < Grape::Entity
   expose(:distance, documentation: { type: 'Float' })
   expose(:trace, documentation: { type: 'String' })
   expose(:destination_id, documentation: { type: 'Integer' })
-  expose(:time, documentation: { type: 'DateTime' }) { |m| m.time.strftime("%H:%M") }
+  expose(:time, documentation: { type: 'DateTime' }) { |m| m.time && m.time.strftime("%H:%M") }
   expose(:out_of_window, documentation: { type: 'Boolean' })
   expose(:out_of_capacity, documentation: { type: 'Boolean' })
   expose(:out_of_drive_time, documentation: { type: 'Boolean' })
