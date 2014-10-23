1ff4ccd1674c7f286d332869d9bfb7921585fc20
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-10-23 13:58:27 +0200

    Fake select2 initialisation on destinations list

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -64,11 +64,11 @@ function format_tag(item) {
   var color = $(item.element).attr('data-color');
   var icon = $(item.element).attr('data-icon');
   if (icon && color) {
-    return '<img src="/images/' + icon + '-' + color.substr(1) + '.svg" />&nbsp;' + item.text;
+    return '<img src="/images/' + icon + '-' + color + '.svg" />&nbsp;' + item.text;
   } else if (icon) {
     return '<img src="/images/' + icon + '.svg" />&nbsp;' + item.text;
   } else if (color) {
-    return '<i style="color:' + color + '" class="fa fa-flag" ></i>&nbsp;' + item.text;
+    return '<i style="color:#' + color + '" class="fa fa-flag" ></i>&nbsp;' + item.text;
   } else {
     return item.text;
   }
