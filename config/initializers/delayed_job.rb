ecdedd2e2dcd615bb07da2d7cf16219d85bd18c4
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2015-02-18 16:21:52 +0100

    Log delayed job localy in its own file

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,3 +1,3 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_attempts = 3
-Delayed::Worker.logger = Rails.logger
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
