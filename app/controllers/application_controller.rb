fc209a2dd75368b343197826e1db3a4c688a952c
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2015-03-13 11:33:07 +0100

    Show nice page on AccessDenied, no more error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
+
   def append_info_to_payload(payload)
     super
     # More info for Lograge
