e62afde8a21926cb47c5793de0c1a5ff3b4ac270
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2015-03-27 11:30:19 +0100

    Fix API entities Enum

--- app/api/v01/entities/order_array.rb
+++ app/api/v01/entities/order_array.rb
@@ -2,6 +2,6 @@ class V01::Entities::OrderArray < Grape::Entity
   expose(:id, documentation: { type: Integer })
   expose(:name, documentation: { type: String })
   expose(:base_date, documentation: { type: Date })
-  expose(:length, documentation: { type: Integer })
+  expose(:length, documentation: { type: String, values: ::OrderArray.lengths.keys })
   expose(:orders, using: V01::Entities::Order, documentation: { type: V01::Entities::Order, is_array: true })
 end
