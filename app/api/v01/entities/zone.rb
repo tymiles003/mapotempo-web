dc26995dd64c80426c5159fb485950bb66a4c85c
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2015-04-06 18:01:13 +0200

    Fix zone geojosn output in API

--- app/api/v01/entities/zone.rb
+++ app/api/v01/entities/zone.rb
@@ -5,5 +5,5 @@ class V01::Entities::Zone < Grape::Entity
 
   expose(:id, documentation: { type: Integer })
   expose(:vehicle_id, documentation: { type: Integer })
-  expose(:polygon, documentation: { type: String })
+  expose(:polygon, documentation: { type: 'Json' }) { |m| JSON.parse(m.polygon) }
 end
