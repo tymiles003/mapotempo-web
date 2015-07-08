2d319aed2e745adbb9674d78f6799aa344b63adb
Fabien Girard <fabien@mapotempo.com>
2015-07-08 14:44:27 +0200

    Redirect unsupported browser without IE special case

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,6 +1,5 @@
 Rails.configuration.middleware.use Browser::Middleware do
   if not request.env['PATH_INFO'].start_with?('/api/')
-    redirect_to unsupported_browser_path(browser: :ie) if browser.ie?
     redirect_to unsupported_browser_path(browser: :modern) if !browser.modern?
   end
 end
