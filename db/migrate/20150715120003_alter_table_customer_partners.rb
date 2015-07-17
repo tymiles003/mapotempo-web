fc297aef408d5098f5007023898edc2b465c743e
Fabien Girard <fabien@mapotempo.com>
2015-07-17 12:18:31 +0200

    Change default value for enabling partners on existing customers

--- db/migrate/20150715120003_alter_table_customer_partners.rb
+++ db/migrate/20150715120003_alter_table_customer_partners.rb
@@ -1,8 +1,11 @@
 class AlterTableCustomerPartners < ActiveRecord::Migration
   def up
-    add_column :customers, :enable_tomtom, :boolean, default: false, null: false
-    add_column :customers, :enable_masternaut, :boolean, default: false, null: false
-    add_column :customers, :enable_alyacom, :boolean, default: false, null: false
+    add_column :customers, :enable_tomtom, :boolean, default: true, null: false
+    add_column :customers, :enable_masternaut, :boolean, default: true, null: false
+    add_column :customers, :enable_alyacom, :boolean, default: true, null: false
+    change_column_default(:customers, :enable_tomtom, false)
+    change_column_default(:customers, :enable_masternaut, false)
+    change_column_default(:customers, :enable_alyacom, false)
   end
 
   def down
