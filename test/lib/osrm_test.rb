cfad04f7115f8cbd99ca7e8d299f05c487e82f10
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2015-08-25 14:28:07 +0200

    Workaround webmock + addressable using hash no working with duplicate params in osrm_test

--- test/lib/osrm_test.rb
+++ test/lib/osrm_test.rb
@@ -8,32 +8,46 @@ class OsrmTest < ActionController::TestCase
 
   test 'should compute matrix' do
     begin
-      uri_template = Addressable::Template.new('localhost:5000/table?loc=45.755932,4.850413')
-      stub_table = stub_request(:get, uri_template).to_return(File.new(File.expand_path('../', __FILE__) + '/osrm/table-1.json').read)
+      points = [[45.750569, 4.839445], [45.763661, 4.851408], [45.755932, 4.850413]]
 
-      matrix = @osrm.matrix(routers(:router_one).url, [[45.750569, 4.839445], [45.763661, 4.851408], [45.755932, 4.850413]])
+      stubs_table = points.collect{ |point|
+        # Workaround webmock + addressable using hash no working with duplicate params
+        uri_template = Addressable::Template.new('localhost:5000/table?loc=' + point.join(','))
+        stub_table = stub_request(:get, uri_template).to_return(File.new(File.expand_path('../', __FILE__) + '/osrm/table-1.json').read)
+      }
+
+      matrix = @osrm.matrix(routers(:router_one).url, points)
       assert_equal 3, matrix.size
       assert_equal 3, matrix[0].size
     ensure
-      remove_request_stub(stub_table)
+      stubs_table.each{ |stub_table| remove_request_stub(stub_table) }
     end
   end
 
   test 'should compute matrix on impassable' do
     begin
-      uri_template = Addressable::Template.new('localhost:5000/viaroute?loc=42.161697,9.13818{&other*}')
-      stub_viaroute = stub_request(:get, uri_template).to_return(File.new(File.expand_path('../', __FILE__) + '/osrm/viaroute-impassable.json').read)
+      points = [[46.634056, 2.547283], [42.161697, 9.138183]]
 
-      uri_template = Addressable::Template.new('localhost:5000/table?loc=42.161697,9.138183')
-      stub_table = stub_request(:get, uri_template).to_return(File.new(File.expand_path('../', __FILE__) + '/osrm/table-impassable.json').read)
+      stubs = points.collect{ |point|
+        # Workaround webmock + addressable using hash no working with duplicate params
+        uri_template = Addressable::Template.new('localhost:5000/viaroute?alt=false&loc=' + point.join(',') + '&output=json')
+        stub_viaroute = stub_request(:get, uri_template).to_return(File.new(File.expand_path('../', __FILE__) + '/osrm/viaroute-impassable.json').read)
 
-      impassable = @osrm.compute(routers(:router_one).url, 46.63487, 2.54804, 42.161697, 9.138183)
+        uri_template = Addressable::Template.new('localhost:5000/table?loc=' + point.join(','))
+        stub_table = stub_request(:get, uri_template).to_return(File.new(File.expand_path('../', __FILE__) + '/osrm/table-impassable.json').read)
+
+        [stub_viaroute, stub_table]
+      }
+
+      impassable = @osrm.compute(routers(:router_one).url, *points.flatten)
       assert_not impassable[2] # no trace
 
-      matrix = @osrm.matrix(routers(:router_one).url, [[46.634056, 2.547283], [42.161697, 9.138183]])
+      matrix = @osrm.matrix(routers(:router_one).url, points)
     ensure
-      remove_request_stub(stub_viaroute)
-      remove_request_stub(stub_table)
+      stubs.each{ |stub|
+        remove_request_stub(stub[0])
+        remove_request_stub(stub[1])
+      }
     end
   end
 
