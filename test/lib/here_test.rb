735b801f8b09c7d625b966b3e5163450cf1ee417
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2015-08-25 09:53:50 +0200

    Clean Here test

--- test/lib/here_test.rb
+++ test/lib/here_test.rb
@@ -2,15 +2,13 @@ require 'here'
 
 class HereTest < ActionController::TestCase
   setup do
-    Mapotempo::Application.config.here.url.gsub!('https', 'http')
-
     @here = Mapotempo::Application.config.here
     @customer = customers(:customer_one)
   end
 
   test 'should compute route' do
     begin
-      uri_template = Addressable::Template.new('route.nlp.nokia.com/routing/{api_version}/calculateroute.json?alternatives=0&app_code={app_code}&app_id={app_id}&mode=fastest%3Btruck%3Btraffic:disabled&representation=display&resolution=1&routeAttributes=summary,shape&truckType=truck&waypoint0=geo!45.750569,4.839445&waypoint1=geo!45.763661,4.851408')
+      uri_template = Addressable::Template.new('https://route.nlp.nokia.com/routing/{api_version}/calculateroute.json?alternatives=0&app_code={app_code}&app_id={app_id}&mode=fastest%3Btruck%3Btraffic:disabled&representation=display&resolution=1&routeAttributes=summary,shape&truckType=truck&waypoint0=geo!45.750569,4.839445&waypoint1=geo!45.763661,4.851408')
       stub = stub_request(:get, uri_template).to_return(File.new(File.expand_path('../', __FILE__) + '/route.nlp.nokia.com/calculateroute.json').read)
 
       trace = @here.compute(45.750569, 4.839445, 45.763661, 4.851408)
@@ -22,7 +20,7 @@ class HereTest < ActionController::TestCase
 
   test 'should compute matrix' do
     begin
-      uri_template = Addressable::Template.new('route.nlp.nokia.com/routing/{api_version}/calculatematrix.json?app_code={app_code}&app_id={app_id}&destination0=45.75057,4.83945&destination1=45.76366,4.85141&destination2=45.75593,4.85041&mode=fastest%3Btruck%3Btraffic:disabled&start0=45.75057,4.83945&start1=45.76366,4.85141&start2=45.75593,4.85041')
+      uri_template = Addressable::Template.new('https://route.nlp.nokia.com/routing/{api_version}/calculatematrix.json?app_code={app_code}&app_id={app_id}&destination0=45.75057,4.83945&destination1=45.76366,4.85141&destination2=45.75593,4.85041&mode=fastest%3Btruck%3Btraffic:disabled&start0=45.75057,4.83945&start1=45.76366,4.85141&start2=45.75593,4.85041')
       stub = stub_request(:get, uri_template).to_return(File.new(File.expand_path('../', __FILE__) + '/route.nlp.nokia.com/calculatematrix.json').read)
 
       matrix = @here.matrix([[45.750569, 4.839445], [45.763661, 4.851408], [45.755932, 4.850413]])
