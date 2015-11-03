ca3451a3d2b603a89d887b7e3f410c26fe77dfcc
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2015-11-03 17:13:54 +0100

    Fix migration optimization_time

--- db/migrate/20151027103159_alter_customer_optimization_time.rb
+++ db/migrate/20151027103159_alter_customer_optimization_time.rb
@@ -1,15 +1,19 @@
 class AlterCustomerOptimizationTime < ActiveRecord::Migration
   def up
     Customer.find_each{|customer|
-      customer.optimization_time = customer.optimization_time / 1000
-      customer.save!
+      if !customer.optimization_time.nil?
+        customer.optimization_time /= 1000
+        customer.save!
+      end
     }
   end
 
   def down
     Customer.find_each{|customer|
-      customer.optimization_time = customer.optimization_time * 1000
-      customer.save!
+      if !customer.optimization_time.nil?
+        customer.optimization_time *= 1000
+        customer.save!
+      end
     }
   end
 end
