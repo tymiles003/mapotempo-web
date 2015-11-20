a78028a507647c887659c5867bb32c3de001c332
Fabien Girard <fabien@mapotempo.com>
2015-11-20 12:13:35 +0100

    Api: fix test get tomtom ids

--- test/api/v01/customers_test.rb
+++ test/api/v01/customers_test.rb
@@ -74,7 +74,7 @@ class V01::CustomerTest < ActiveSupport::TestCase
     assert last_response.ok?, last_response.body
   end
 
-  test 'Delete job' do
+  test 'should delete job' do
     assert_difference('Delayed::Backend::ActiveRecord::Job.count', -1) do
       delete api("#{@customer.id}/job/#{@customer.job_destination_geocoding_id}")
       assert last_response.ok?, last_response.body
@@ -86,10 +86,9 @@ class V01::CustomerTest < ActiveSupport::TestCase
     stub_table = stub_request(:get, uri_template).to_return(File.new(File.expand_path('../../../lib/', __FILE__) + '/tomtom/tomtom-1-wsdl.xml').read)
 
     uri_template = Addressable::Template.new('https://soap.business.tomtom.com/v1.25/objectsAndPeopleReportingService')
-    stub_table = stub_request(:post, uri_template).with(
-      :body => "<?xml version=\"1.0\" encoding=\"UTF-8\"?><env:Envelope xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:tns=\"http://connect.webfleet.tomtomwork.com/services\" xmlns:env=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:ins0=\"http://connect.webfleet.tomtomwork.com/transfer\" xmlns:ins1=\"http://connect.webfleet.tomtomwork.com/transfer/objects\" xmlns:ins2=\"http://connect.webfleet.tomtomwork.com/parameters/auxdevicedata\" xmlns:ins3=\"http://connect.webfleet.tomtomwork.com/transfer/positions\" xmlns:ins4=\"http://connect.webfleet.tomtomwork.com/transfer/orders\" xmlns:ins5=\"http://connect.webfleet.tomtomwork.com/transfer/addresses\" xmlns:ins6=\"http://connect.webfleet.tomtomwork.com/parameters\" xmlns:ins7=\"http://connect.webfleet.tomtomwork.com/parameters/objects\"><env:Body><tns:showObjectReport><aParm><apiKey xsi:nil=\"true\"/><accountName xsi:nil=\"true\"/><userName xsi:nil=\"true\"/><password xsi:nil=\"true\"/></aParm><gParm></gParm></tns:showObjectReport></env:Body></env:Envelope>",
-      :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Length'=>'1030', 'Content-Type'=>'application/soap+xml;charset=UTF-8', 'Soapaction'=>'"showObjectReport"', 'User-Agent'=>'Ruby'}
-    ).to_return(File.new(File.expand_path('../../../lib/', __FILE__) + '/tomtom/tomtom-1.xml').read)
+    stub_table = stub_request(:post, uri_template).with { |request|
+      request.body.include?("showObjectReport")
+    }.to_return(File.new(File.expand_path('../../../lib/', __FILE__) + '/tomtom/tomtom-1.xml').read)
 
     get api("#{@customer.id}/tomtom_ids")
     assert last_response.ok?, last_response.body
