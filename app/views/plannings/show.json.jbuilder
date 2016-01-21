tymiles003/mapotempo-web

commit 1a060321a54a2b5c63274efb0053a037f03f4490

Move display_start_time to helper

app/views/plannings/show.json.jbuilder
@@ -41,7 +41,7 @@ else
     json.store_start do
       json.extract! route.vehicle_usage.default_store_start, :id, :name, :street, :postalcode, :city, :country, :lat, :lng, :color, :icon
       (json.time l(route.start, format: :hour_minute)) if route.start
-      (json.display_time l(route.display_start_time, format: :hour_minute)) if route.display_start_time
+      (json.with_service_time l(display_start_time(route), format: :hour_minute)) if route.vehicle_usage.service_time_start
       (json.geocoded true) if !route.vehicle_usage.default_store_start.lat.nil? && !route.vehicle_usage.default_store_start.lng.nil?
       (json.error true) if route.vehicle_usage.default_store_start.lat.nil? || route.vehicle_usage.default_store_start.lng.nil?
     end if route.vehicle_usage && route.vehicle_usage.default_store_start
@@ -119,7 +119,7 @@ else
     json.store_stop do
       json.extract! route.vehicle_usage.default_store_stop, :id, :name, :street, :postalcode, :city, :country, :lat, :lng, :color, :icon
       (json.time l(route.end, format: :hour_minute)) if route.end
-      (json.display_time l(route.display_end_time, format: :hour_minute)) if route.display_end_time
+      (json.with_service_time l(display_end_time(route), format: :hour_minute)) if route.vehicle_usage.service_time_end
       (json.geocoded true) if !route.vehicle_usage.default_store_stop.lat.nil? && !route.vehicle_usage.default_store_stop.lng.nil?
       (json.no_path true) if route.distance > 0 && !route.vehicle_usage.default_store_stop.lat.nil? && !route.vehicle_usage.default_store_stop.lng.nil? && !route.stop_trace
       (json.error true) if (route.vehicle_usage.default_store_stop.lat.nil? || route.vehicle_usage.default_store_stop.lng.nil?) || (route.distance > 0 && !route.vehicle_usage.default_store_stop.lat.nil? && !route.vehicle_usage.default_store_stop.lng.nil? && !route.stop_trace)

app/helpers/routes_helper.rb
@@ -1,4 +1,4 @@
-# Copyright © Mapotempo, 2013-2014
+# Copyright © Mapotempo, 2013-2016
 #
 # This file is part of Mapotempo.
 #
@@ -16,4 +16,11 @@
 # <http://www.gnu.org/licenses/agpl.html>
 #
 module RoutesHelper
+  def display_start_time(route)
+    route.start + route.service_time_start_value if route.start && route.service_time_start_value
+  end
+
+  def display_end_time(route)
+    route.end + route.service_time_end_value if route.end && route.service_time_end_value
+  end
 end

app/models/route.rb
@@ -65,14 +65,6 @@ class Route < ActiveRecord::Base
     vehicle_usage.default_service_time_end - Time.utc(2000, 1, 1, 0, 0) if vehicle_usage && vehicle_usage.default_service_time_end
   end
 
-  def display_start_time
-    self.start + service_time_start_value if self.start && service_time_start_value
-  end
-
-  def display_end_time
-    self.end + service_time_end_value if self.end && service_time_end_value
-  end
-
   def plan(departure = nil)
     self.out_of_date = false
     self.distance = 0

app/templates/plannings/_edit.mustache.erb
@@ -164,7 +164,7 @@
             {{/no_path}}
             <span class="info">
               {{#time}}
-                {{time}}{{#display_time}}&nbsp;-&nbsp;{{display_time}}{{/display_time}}
+                {{time}}{{#with_service_time}}&nbsp;-&nbsp;{{with_service_time}}{{/with_service_time}}
               {{/time}}
             </span>
             <span class="name" title="{{name}}">{{name}}</span>
@@ -268,7 +268,7 @@
             {{/no_path}}
             <span class="info">
               {{#time}}
-                {{time}}{{#display_time}}&nbsp;-&nbsp;{{display_time}}{{/display_time}}
+                {{time}}{{#with_service_time}}&nbsp;-&nbsp;{{with_service_time}}{{/with_service_time}}
               {{/time}}
             </span>
             <span class="name" title="{{name}}">{{name}}</span>

commit add9663f41ae1bbc741e106617fdcbe39f0b0029

Planning: Service Time - Fix error introduced in 1a060321a54a2b5c63274efb0053a037f03f4490

app/views/plannings/show.json.jbuilder
@@ -41,7 +41,7 @@ else
     json.store_start do
       json.extract! route.vehicle_usage.default_store_start, :id, :name, :street, :postalcode, :city, :country, :lat, :lng, :color, :icon
       (json.time l(route.start, format: :hour_minute)) if route.start
-      (json.with_service_time l(display_start_time(route), format: :hour_minute)) if route.vehicle_usage.service_time_start
+      (json.with_service_time l(display_start_time(route), format: :hour_minute)) if display_start_time(route)
       (json.geocoded true) if !route.vehicle_usage.default_store_start.lat.nil? && !route.vehicle_usage.default_store_start.lng.nil?
       (json.error true) if route.vehicle_usage.default_store_start.lat.nil? || route.vehicle_usage.default_store_start.lng.nil?
     end if route.vehicle_usage && route.vehicle_usage.default_store_start
@@ -119,7 +119,7 @@ else
     json.store_stop do
       json.extract! route.vehicle_usage.default_store_stop, :id, :name, :street, :postalcode, :city, :country, :lat, :lng, :color, :icon
       (json.time l(route.end, format: :hour_minute)) if route.end
-      (json.with_service_time l(display_end_time(route), format: :hour_minute)) if route.vehicle_usage.service_time_end
+      (json.with_service_time l(display_end_time(route), format: :hour_minute)) if display_end_time(route)
       (json.geocoded true) if !route.vehicle_usage.default_store_stop.lat.nil? && !route.vehicle_usage.default_store_stop.lng.nil?
       (json.no_path true) if route.distance > 0 && !route.vehicle_usage.default_store_stop.lat.nil? && !route.vehicle_usage.default_store_stop.lng.nil? && !route.stop_trace
       (json.error true) if (route.vehicle_usage.default_store_stop.lat.nil? || route.vehicle_usage.default_store_stop.lng.nil?) || (route.distance > 0 && !route.vehicle_usage.default_store_stop.lat.nil? && !route.vehicle_usage.default_store_stop.lng.nil? && !route.stop_trace)