b37edbadb4125dc947cd504bc1b5eecd565e46a9
Nicolas Gonzalez <nicolas@mapotempo.com>
2016-01-25 17:36:26 +0100

    Vehicle: Authorize E-Mail field

--- app/api/v01/entities/vehicle.rb
+++ app/api/v01/entities/vehicle.rb
@@ -21,6 +21,7 @@ class V01::Entities::Vehicle < Grape::Entity
   end
 
   expose(:id, documentation: { type: Integer })
+  expose(:contact_email, documentation: { type: String })
   expose(:ref, documentation: { type: String })
   expose(:name, documentation: { type: String })
   expose(:emission, documentation: { type: Integer })
