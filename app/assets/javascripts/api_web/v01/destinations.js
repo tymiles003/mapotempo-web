361bc307379c045a04432bbb771db2098b0dad62
Fabien Girard <fabien@mapotempo.com>
2016-02-03 11:46:31 +0100

    Api-web: fix store marker creation with specific icon size

--- app/assets/javascripts/api_web/v01/destinations.js
+++ app/assets/javascripts/api_web/v01/destinations.js
@@ -53,10 +53,10 @@ var api_web_v01_display_destinations_ = function(api, map, data) {
     var licon;
     if (options.store) {
       licon = L.divIcon({
-        html: '<i class="fa ' + (options.icon || 'fa-home') + ' ' + map.iconSize[store.icon_size || 'large'].name + ' store-icon" style="color: ' + (options.color || 'black') + ';"></i>',
-        iconSize: new L.Point(map.iconSize[store.icon_size || 'large'].size, map.iconSize[store.icon_size || 'large'].size),
-        iconAnchor: new L.Point(map.iconSize[store.icon_size || 'large'].size / 2, map.iconSize[store.icon_size || 'large'].size / 2),
-        popupAnchor: new L.Point(0, -Math.trunc(map.iconSize[store.icon_size || 'large'].size / 2.5)),
+        html: '<i class="fa ' + (options.icon || 'fa-home') + ' ' + map.iconSize[options.icon_size || 'large'].name + ' store-icon" style="color: ' + (options.color || 'black') + ';"></i>',
+        iconSize: new L.Point(map.iconSize[options.icon_size || 'large'].size, map.iconSize[options.icon_size || 'large'].size),
+        iconAnchor: new L.Point(map.iconSize[options.icon_size || 'large'].size / 2, map.iconSize[options.icon_size || 'large'].size / 2),
+        popupAnchor: new L.Point(0, -Math.trunc(map.iconSize[options.icon_size || 'large'].size / 2.5)),
         className: 'store-icon-container'
       });
     } else {
