2ebc8240e2e88a2ad63c7672ac14f887df6b1a39
Fabien Girard <fabien@mapotempo.com>
2016-02-11 14:02:39 +0100

    Group queries to improve loading of destinations list

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -26,7 +26,7 @@ class DestinationsController < ApplicationController
 
   def index
     @customer = current_user.customer
-    @destinations = current_user.customer.destinations
+    @destinations = current_user.customer.destinations.includes(:tags, visits: :tags)
     @tags = current_user.customer.tags
     respond_to do |format|
       format.html
