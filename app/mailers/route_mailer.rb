17b60780b40aa7cddd41603e67b1226f6140c121
Nicolas Gonzalez <nicolas@mapotempo.com>
2016-02-24 16:24:58 +0100

    Route Mailer: Send KMZ File, Fix Buggy Content-Type

--- app/mailers/route_mailer.rb
+++ app/mailers/route_mailer.rb
@@ -1,13 +1,8 @@
 class RouteMailer < ApplicationMailer
   def send_kmz_route user, vehicle, route, filename, attachment
-    attachments['kmz'] = {
-      mime_type: 'application/vnd.google-earth.kmz',
-      transfer_encoding: :binary,
-      content: attachment.force_encoding('BINARY'),
-      content_disposition: "attachment; filename=\"#{filename}\";"
-    }
+    attachments[filename] = { transfer_encoding: :binary, content: attachment.force_encoding('BINARY') }
     mail from: user.email, to: vehicle.contact_email, subject: "[#{user.customer.reseller.name}] #{filename}" do |format|
-      format.html { render 'route_mailer/send_kmz_route', locals: { vehicle: vehicle, route: route } }
+      format.text { render 'route_mailer/send_kmz_route', locals: { vehicle: vehicle, route: route } }
     end
   end
 end
