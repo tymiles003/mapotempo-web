320464142ccff62568bd31b81d4d3360caa08de6
Nicolas Gonzalez <nicolas@mapotempo.com>
2016-03-08 11:16:43 +0100

    Devices: Remove Buggy Cache Namespace

--- config/initializers/devices.rb
+++ config/initializers/devices.rb
@@ -5,7 +5,7 @@ Mapotempo::Application.config.devices.masternaut.api_url = 'https://masternaut.e
 Mapotempo::Application.config.devices.orange.api_url = 'https://orange.example.com'
 Mapotempo::Application.config.devices.tomtom.api_url = 'https://tomtom.example.com/v1.25'
 Mapotempo::Application.config.devices.tomtom.api_key = nil
-Mapotempo::Application.config.devices.cache_object = ActiveSupport::Cache::FileStore.new File.join(Dir.tmpdir, 'devices'), namespace: self.class.name.tableize, expires_in: 30
+Mapotempo::Application.config.devices.cache_object = ActiveSupport::Cache::FileStore.new File.join(Dir.tmpdir, 'devices'), namespace: 'devices', expires_in: 30
 
 # TomTom: Fetch WSDL when starting service
 Mapotempo::Application.config.devices.tomtom.fetch_wsdl
