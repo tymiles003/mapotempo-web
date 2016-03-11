tymiles003/mapotempo-web

commit 81bb85818db30842909d69f3d14383de633f3df6

Devices: Pass Customer / Route As Method Argument
Devices: Move Files To Devices Subfolder

app/services/device_service.rb
@@ -23,18 +23,17 @@ class DeviceService
     @cache_object = Mapotempo::Application.config.devices.cache_object
     @service_name = self.class.name.gsub("Service", "").downcase
     @service = Mapotempo::Application.config.devices[service_name]
-    @service.set_params params
   end
 
-  def send_route options
-    service.send_route options
-    options[:route].update! last_sent_at: Time.now
-    return options[:route].last_sent_at
+  def send_route route, options={}
+    service.send_route customer, route, options
+    route.update! last_sent_at: Time.now
+    return route.last_sent_at
   end
 
-  def clear_route options
-    service.clear_route options
-    options[:route].update! last_sent_at: nil
+  def clear_route route, options={}
+    service.clear_route customer, route, options
+    route.update! last_sent_at: nil
   end
 
   private

lib/devices/device_helpers.rb
@@ -1,9 +1,8 @@
 module Devices
   module Helpers
-
     def device_send_route options={}
       route = Route.for_customer(@customer).find params[:route_id]
-      service.send_route options.merge(route: route)
+      service.send_route route, options
       present route, with: V01::Entities::DeviceRouteLastSentAt
     end
 
@@ -11,13 +10,13 @@ module Devices
       planning = @customer.plannings.find params[:planning_id]
       routes = planning.routes.select(&:vehicle_usage)
       routes = routes.select{|route| route.vehicle_usage.vehicle.send(options[:device_id]) } if options[:device_id]
-      routes.each{|route| service.send_route options.merge(route: route) }
+      routes.each{|route| service.send_route(route, options) }
       present routes, with: V01::Entities::DeviceRouteLastSentAt
     end
 
     def device_clear_route options={}
       route = Route.for_customer(@customer).find params[:route_id]
-      service.clear_route options.merge(route: route)
+      service.clear_route(route, options)
       present route, with: V01::Entities::DeviceRouteLastSentAt
     end
 
@@ -25,7 +24,7 @@ module Devices
       planning = @customer.plannings.find params[:planning_id]
       routes = planning.routes.select(&:vehicle_usage)
       routes = routes.select{|route| route.vehicle_usage.vehicle.send(options[:device_id]) } if options[:device_id]
-      routes.each{|route| service.clear_route options.merge(route: route) }
+      routes.each{|route| service.clear_route(route, options) }
       present routes, with: V01::Entities::DeviceRouteLastSentAt
     end
 
@@ -47,7 +46,7 @@ module Devices
       username = params[:teksat_username]    ? params[:teksat_username]    : customer.try(:teksat_username)
       password = params[:teksat_password]    ? params[:teksat_password]    : customer.try(:teksat_password)
       if params[:check_only].to_i == 1 || !session[:teksat_ticket_id] || (Time.now - Time.at(session[:teksat_authenticated_at])) > 3.hours
-        ticket_id = TeksatService.new(customer: customer).authenticate({ url: url, customer_id: cust_id, username: username, password: password})
+        ticket_id = TeksatService.new(customer: customer).authenticate(url: url, customer_id: cust_id, username: username, password: password)
         session[:teksat_ticket_id] = ticket_id
         session[:teksat_authenticated_at] = Time.now.to_i
       end

lib/devices/orange.rb
@@ -16,17 +16,15 @@
 # <http://www.gnu.org/licenses/agpl.html>
 #
 class Orange < DeviceBase
-
   require "builder" # XML
   require "addressable"
 
-  def test_list params
-    @auth = params.slice :user, :password
-    send_request list_operations({ eqpid: "", dtdeb: Time.now.beginning_of_day, dtfin: Time.now.end_of_day })
+  def test_list customer, params
+    send_request list_operations(customer, { auth: params.slice(:user, :password) })
   end
 
-  def list_devices
-    response = send_request(get_vehicles)
+  def list_devices customer
+    response = send_request(get_vehicles(customer))
     if response.code.to_i == 200
       vehicle_infos = []
       Nokogiri::XML(response.body).xpath("//vehicle").each_with_object({}) do |item, hash|
@@ -41,19 +39,17 @@ class Orange < DeviceBase
     end
   end
 
-  def send_route options
-    @route = options[:route]
-    send_request(send_xml_file)
+  def send_route customer, route, options={}
+    send_request send_xml_file(customer, route)
   end
 
-  def clear_route options
+  def clear_route customer, route, options={}
     # Not supported by Garmin 590 -- Garmin Dezl and Nuvi only
-    @route = options[:route]
-    send_request send_xml_file(delete: true)
+    send_request send_xml_file(customer, route, { delete: true })
   end
 
-  def get_vehicles_pos
-    response = send_request(get_positions)
+  def get_vehicles_pos customer
+    response = send_request(get_positions(customer))
     if response.code.to_i == 200
       vehicle_infos = []
       Nokogiri::XML(response.body).xpath("//position").each_with_object({}) do |item, hash|
@@ -78,10 +74,10 @@ class Orange < DeviceBase
     end
   end
 
-  def net_request options
+  def net_request customer, options
     # Auth
-    if auth
-      user, password = auth[:user], auth[:password]
+    if options[:auth]
+      user, password = options[:auth][:user], options[:auth][:password]
     else
       user, password = customer.orange_user, customer.orange_password
     end
@@ -99,26 +95,26 @@ class Orange < DeviceBase
     http.request request
   end
 
-  def get_positions
-    net_request path: "/webservices/getpositions.php", params: { ext: "xml" }
+  def get_positions customer
+    net_request customer, { path: "/webservices/getpositions.php", params: { ext: "xml" } }
   end
 
-  def get_vehicles
-    net_request path: "/webservices/getvehicles.php", params: { ext: "xml" }
+  def get_vehicles customer
+    net_request customer, { path: "/webservices/getvehicles.php", params: { ext: "xml" } }
   end
 
-  def list_operations options
-    net_request path: "/pnd/index.php", params: { ext: "xml", ref: "", vehid: "", typ: "mis" }
+  def list_operations customer, options
+    net_request customer, options.merge(path: "/pnd/index.php", params: { ext: "xml", ref: "", vehid: "", typ: "mis", eqpid: "", dtdeb: Time.now.beginning_of_day, dtfin: Time.now.end_of_day })
   end
 
-  def send_xml_file options={}
-    f = Tempfile.new Time.now.to_i.to_s ; f.write to_xml(options) ; f.rewind
+  def send_xml_file customer, route, options={}
+    f = Tempfile.new Time.now.to_i.to_s ; f.write to_xml(route, options) ; f.rewind
     response = RestClient::Request.execute method: :post, user: customer.orange_user, password: customer.orange_password, url: api_url + "/pnd/index.php", payload: { multipart: true, file: f }
     f.unlink
     return response
   end
 
-  def to_xml options={}
+  def to_xml route, options={}
     xml = ::Builder::XmlMarkup.new indent: 2
     xml.instruct!
     xml.tag! :ROOT do
@@ -126,12 +122,12 @@ class Orange < DeviceBase
       xml.tag! :transmit, Time.now.strftime("%d/%m/%Y %H:%M")
       xml.tag! :zone, nil, type: "dest", ref: route.id, eqpid: route.vehicle_usage.vehicle.orange_id, drivername: nil, vehid: nil, badge: nil
       xml.tag! :zone, nil, type: "mission", ref: route.id, lang: nil, title: "Mission #{route.id}", txt: route.planning.name,
-        prevmisdeb: p_time(route.start).strftime("%d/%m/%Y %H:%M"), prevmisfin: p_time(route.end).strftime("%d/%m/%Y %H:%M")
+        prevmisdeb: p_time(route, route.start).strftime("%d/%m/%Y %H:%M"), prevmisfin: p_time(route, route.end).strftime("%d/%m/%Y %H:%M")
       xml.tag! :zone, nil, type: "operation" do
         route.stops.select(&:active?).select(&:position?).sort_by(&:index).each do |stop|
           xml.tag! :operation, nil, options.merge(seq: stop.index, ad1: stop.street, ad2: nil, ad3: nil, ad_zip: stop.postalcode,
             ad_city: stop.city, ad_cntry: "FR", latitude: stop.lat, longitude: stop.lng, title: stop.name, txt: [stop.street, stop.postalcode, stop.city].join(", "),
-            prevopedeb: p_time(stop.open || stop.time).strftime("%d/%m/%Y %H:%M"), prevopefin: p_time(stop.close || stop.time).strftime("%d/%m/%Y %H:%M"))
+            prevopedeb: p_time(route, stop.open || stop.time).strftime("%d/%m/%Y %H:%M"), prevopefin: p_time(route, stop.close || stop.time).strftime("%d/%m/%Y %H:%M"))
         end
       end
     end

lib/devices/teksat.rb
@@ -21,9 +21,8 @@ class Teksat < DeviceBase
 
   attr_accessor :ticket_id
 
-  def authenticate params
-    @auth = params.slice :url, :customer_id, :username, :password
-    response = RestClient.get get_ticket_url
+  def authenticate customer, params
+    response = RestClient.get get_ticket_url(customer, { auth: params.slice(:url, :customer_id, :username, :password) })
     if response.code == 200 && response.strip.length >= 1
       return response.strip
     else
@@ -31,8 +30,8 @@ class Teksat < DeviceBase
     end
   end
 
-  def list_devices
-    response = RestClient.get get_vehicles_url
+  def list_devices customer
+    response = RestClient.get get_vehicles_url(customer)
     if response.code == 200
       Nokogiri::XML(response).xpath("//vehicle").map do |item|
         { id: item["id"], text: "%s %s - %s" % [ item["brand"], item["type"], item["code"] ] }
@@ -42,21 +41,19 @@ class Teksat < DeviceBase
     end
   end
 
-  def send_route options
-    @route = options[:route]
-    send_mission route.start, route.vehicle_usage.default_store_start.lat, route.vehicle_usage.default_store_start.lng
-    route.stops.select(&:active?).select(&:position?).sort_by(&:index).each{|stop| send_mission(stop.time, stop.lat, stop.lng) }
-    send_mission route.end, route.vehicle_usage.default_store_stop.lat, route.vehicle_usage.default_store_stop.lng
+  def send_route customer, route, options={}
+    send_mission customer, route, route.start, route.vehicle_usage.default_store_start.lat, route.vehicle_usage.default_store_start.lng
+    route.stops.select(&:active?).select(&:position?).sort_by(&:index).each{|stop| send_mission(customer, route, stop.time, stop.lat, stop.lng) }
+    send_mission customer, route, route.end, route.vehicle_usage.default_store_stop.lat, route.vehicle_usage.default_store_stop.lng
   end
 
-  def clear_route options
-    @route = options[:route]
-    response = RestClient.get get_missions_url(date: planning_date.strftime("%Y-%m-%d"))
-    Nokogiri::XML(response).xpath("//mission").map{|item| RestClient.get(delete_mission_url(mi_id: item["id"])) }
+  def clear_route customer, route, options={}
+    response = RestClient.get get_missions_url(customer, { date: planning_date(route).strftime("%Y-%m-%d") })
+    Nokogiri::XML(response).xpath("//mission").map{|item| RestClient.get(delete_mission_url(customer, { mi_id: item["id"] })) }
   end
 
-  def get_vehicles_pos
-    response = RestClient.get get_vehicles_pos_url
+  def get_vehicles_pos customer
+    response = RestClient.get get_vehicles_pos_url(customer)
     if response.code == 200
       Nokogiri::XML(response).xpath("//vehicle_pos").map do |item|
         { teksat_vehicle_id: item["v_id"], lat: item["lat"], lng: item["lng"], speed: item["speed"], time: item["data_time"], device_name: item["code"] }
@@ -68,15 +65,15 @@ class Teksat < DeviceBase
 
   private
 
-  def send_mission start_time, lat, lng
+  def send_mission customer, route, start_time, lat, lng
     route_params = {
       mi_v_id: route.vehicle_usage.vehicle.teksat_id,
       mi_label: route.planning.name,
       mi_customer: route.planning.customer.name
     }
-    response = RestClient.get set_mission_url(route_params.merge(
-      mi_begin_date: p_time(start_time).strftime("%Y-%m-%d"),
-      mi_begin_time: p_time(start_time).strftime("%H-%M-%S"),
+    response = RestClient.get set_mission_url(customer, route_params.merge(
+      mi_begin_date: p_time(route, start_time).strftime("%Y-%m-%d"),
+      mi_begin_time: p_time(route, start_time).strftime("%H-%M-%S"),
       mi_begin_latitude: lat,
       mi_begin_longitude: lng
     ))
@@ -85,9 +82,9 @@ class Teksat < DeviceBase
     end
   end
 
-  def get_ticket_url
-    if auth
-      url, customer_id, username, password = auth[:url], auth[:customer_id], auth[:username], auth[:password]
+  def get_ticket_url customer, options={}
+    if options[:auth]
+      url, customer_id, username, password = options[:auth][:url], options[:auth][:customer_id], options[:auth][:username], options[:auth][:password]
     else
       url, customer_id, username, password = customer.teksat_url, customer.teksat_customer_id, customer.teksat_username, customer.teksat_password
     end
@@ -99,31 +96,31 @@ class Teksat < DeviceBase
     ).to_s
   end
 
-  def get_vehicles_url
+  def get_vehicles_url customer
     Addressable::Template.new("http://%s/webservices/map/get-vehicles.jsp{?query*}" % [ customer.teksat_url ]).expand(
       query: { custID: customer.teksat_customer_id, tck: ticket_id }
     ).to_s
   end
 
-  def get_vehicles_pos_url
+  def get_vehicles_pos_url customer
     Addressable::Template.new("http://%s/webservices/map/get-vehicles-pos.jsp{?query*}" % [ customer.teksat_url ]).expand(
       query: { custID: customer.teksat_customer_id, tck: ticket_id }
     ).to_s
   end
 
-  def set_mission_url options
+  def set_mission_url customer, options
     Addressable::Template.new("http://%s/webservices/map/set-mission.jsp{?query*}" % [ customer.teksat_url ]).expand(
       query: options.merge(custID: customer.teksat_customer_id, tck: ticket_id)
     ).to_s
   end
 
-  def get_missions_url options
+  def get_missions_url customer, options
     Addressable::Template.new("http://%s/webservices/map/get-missions.jsp{?query*}" % [ customer.teksat_url ]).expand(
       query: options.merge(custID: customer.teksat_customer_id, tck: ticket_id)
     ).to_s
   end
 
-  def delete_mission_url options
+  def delete_mission_url customer, options
     Addressable::Template.new("http://%s/webservices/map/delete-mission.jsp{?query*}" % [ customer.teksat_url ]).expand(
       query: options.merge(custID: customer.teksat_customer_id, tck: ticket_id)
     ).to_s

lib/devices/tomtom.rb
@@ -55,13 +55,12 @@ class Tomtom < DeviceBase
     'blue' => '#0000FF'
   }
 
-  def test_list params
-    @auth = params.slice :account, :user, :password
-    list_devices
+  def test_list customer, params
+    list_devices customer, { auth: params.slice(:account, :user, :password) }
   end
 
-  def list_devices
-    objects = get savon_client_objects, :show_object_report, {}
+  def list_devices customer, options={}
+    objects = get customer, savon_client_objects, :show_object_report, options
     objects = [objects] if objects.is_a?(Hash)
     objects.select{ |object| !object[:deleted] }.collect do |object|
       {
@@ -71,8 +70,8 @@ class Tomtom < DeviceBase
     end
   end
 
-  def get_vehicles_pos
-    objects = get savon_client_objects, :show_object_report, {}
+  def get_vehicles_pos customer
+    objects = get customer, savon_client_objects, :show_object_report
     objects = [objects] if objects.is_a?(Hash)
     objects.select{ |object| !object[:deleted] }.collect do |object|
       {
@@ -85,8 +84,8 @@ class Tomtom < DeviceBase
     end
   end
 
-  def list_vehicles
-    objects = get savon_client_objects, :show_vehicle_report, {}
+  def list_vehicles customer
+    objects = get customer, savon_client_objects, :show_vehicle_report
     objects = [objects] if objects.is_a?(Hash)
     objects.select{ |object| !object[:deleted] }.collect do |object|
       hash = {
@@ -102,8 +101,8 @@ class Tomtom < DeviceBase
     end
   end
 
-  def list_addresses
-    addresss = get savon_client_address, :show_address_report, {}
+  def list_addresses customer
+    addresss = get customer, savon_client_address, :show_address_report
     addresss = [addresss] if addresss.is_a?(Hash)
     addresss.select{ |object| !object[:deleted] }.collect do |address|
       {
@@ -122,13 +121,12 @@ class Tomtom < DeviceBase
     end
   end
 
-  def send_route options
-    @route = options[:route]
+  def send_route customer, route, options={}
     case options[:type].to_sym
       when :orders
         position = route.vehicle_usage.default_store_stop
         if position && !position.lat.nil? && !position.lng.nil?
-          sendDestinationOrder position, -2, route.vehicle_usage.default_store_stop && route.vehicle_usage.default_store_stop.name || "#{position.lat} #{position.lng}", route.start
+          sendDestinationOrder customer, route, position, -2, route.vehicle_usage.default_store_stop && route.vehicle_usage.default_store_stop.name || "#{position.lat} #{position.lng}", route.start
         end
         route.stops.select(&:active).reverse_each{ |stop|
           position = stop if stop.position?
@@ -143,12 +141,12 @@ class Tomtom < DeviceBase
               stop.comment,
               stop.phone_number,
             ].compact.join(' ').strip
-            sendDestinationOrder position, stop.id, description, stop.time
+            sendDestinationOrder customer, route, position, stop.id, description, stop.time
           end
         }
         position = route.vehicle_usage.default_store_start
         if position && !position.lat.nil? && !position.lng.nil?
-          sendDestinationOrder position, -1, route.vehicle_usage.default_store_start && route.vehicle_usage.default_store_start.name || "#{position.lat} #{position.lng}", route.start
+          sendDestinationOrder customer, route, position, -1, route.vehicle_usage.default_store_start && route.vehicle_usage.default_store_start.name || "#{position.lat} #{position.lng}", route.start
         end
 
       when :waypoints
@@ -186,13 +184,12 @@ class Tomtom < DeviceBase
         }
         position = route.vehicle_usage.default_store_stop if route.vehicle_usage.default_store_stop && !route.vehicle_usage.default_store_stop.lat.nil? && !route.vehicle_usage.default_store_stop.lng.nil?
         description = route.ref || (waypoints[-1] && waypoints[-1][:description]) || "#{waypoints[-1][:lat]} #{waypoints[-1][:lng]}"
-        sendDestinationOrder position, route.vehicle_usage.id, description, route.start, waypoints
+        sendDestinationOrder customer, route, position, route.vehicle_usage.id, description, route.start, waypoints
     end
   end
 
-  def clear_route options
-    @route = options[:route]
-    get savon_client_orders, :clear_orders,
+  def clear_route customer, route, options={}
+    get customer, savon_client_orders, :clear_orders, options, {
       deviceToClear: {
         markDeleted: 'true',
       },
@@ -200,14 +197,15 @@ class Tomtom < DeviceBase
         deviceToClear: {
           objectUid: route.vehicle_usage.vehicle.tomtom_id,
         }
-     }
+      }
+    }
   end
 
   private
 
-  def get client, operation, message={}
-    if auth
-      account, username, password = auth[:account], auth[:user], auth[:password]
+  def get customer, client, operation, options={}, message={}
+    if options[:auth]
+      account, username, password = options[:auth][:account], options[:auth][:user], options[:auth][:password]
     else
       account, username, password = customer.tomtom_account, customer.tomtom_user, customer.tomtom_password
     end
@@ -255,10 +253,10 @@ class Tomtom < DeviceBase
     end
   end
 
-  def sendDestinationOrder position, orderid, description, time, waypoints = nil
+  def sendDestinationOrder customer, route, position, orderid, description, time, waypoints=nil
     time_2000 = Time.new(2000, 1, 1, 0, 0, 0, '+00:00').to_i
     objectuid = route.vehicle_usage.vehicle.tomtom_id
-    date = planning_date
+    date = planning_date(route)
     unique_base_order_id = (orderid.to_s + Time.now.to_i.to_s).to_i.to_s(36)
     params = {
       dstOrderToSend: {
@@ -302,7 +300,7 @@ class Tomtom < DeviceBase
         }
       }}
     end
-    get savon_client_orders, :send_destination_order, params
+    get customer, savon_client_orders, :send_destination_order, params
   end
 
 end

app/api/v01/api.rb
@@ -18,7 +18,7 @@
 class V01::Api < Grape::API
   helpers do
 
-    require Rails.root.join("lib/device_helpers")
+    require Rails.root.join("lib/devices/device_helpers")
     include Devices::Helpers
 
     def session

app/api/v01/vehicles.rb
@@ -17,7 +17,7 @@
 #
 require 'coerce'
 
-require Rails.root.join("lib/device_helpers")
+require Rails.root.join("lib/devices/device_helpers")
 include Devices::Helpers
 
 class V01::Vehicles < Grape::API
@@ -75,33 +75,34 @@ class V01::Vehicles < Grape::API
       optional :ids, type: Array[String], desc: 'Select vehicles by id separated with comma. You can specify ref (not containing comma) instead of id, in this case you have to add "ref:" before each ref, e.g. ref:ref1,ref:ref2,ref:ref3.', coerce_with: CoerceArrayString
     end
     get 'current_position' do
+      customer = current_customer
       vehicles = if params.key?(:ids)
-        current_customer.vehicles.select{ |vehicle|
+        customer.vehicles.select{ |vehicle|
           params[:ids].any?{ |s| ParseIdsRefs.match(s, vehicle) }
         }
       else
-        current_customer.vehicles.load
+        customer.vehicles.load
       end
       positions = []
-      if current_customer.orange?
-        OrangeService.new(customer: current_customer).get_vehicles_pos.each do |item|
+      if customer.orange?
+        OrangeService.new(customer: customer).get_vehicles_pos.each do |item|
           vehicle_id = item.delete :orange_vehicle_id
           vehicle = vehicles.detect{|v| v.orange_id == vehicle_id }
           next if !vehicle
           positions << item.merge(vehicle_id: vehicle.id)
         end
       end
-      if current_customer.teksat?
-        teksat_authenticate current_customer
-        TeksatService.new(customer: current_customer, ticket_id: session[:teksat_ticket_id]).get_vehicles_pos.each do |item|
+      if customer.teksat?
+        teksat_authenticate customer
+        TeksatService.new(customer: customer, ticket_id: session[:teksat_ticket_id]).get_vehicles_pos.each do |item|
           vehicle_id = item.delete :teksat_vehicle_id
           vehicle = vehicles.detect{|v| v.teksat_id == vehicle_id }
           next if !vehicle
           positions << item.merge(vehicle_id: vehicle.id)
         end
       end
-      if current_customer.tomtom?
-        TomtomService.new(customer: current_customer).get_vehicles_pos.each do |item|
+      if customer.tomtom?
+        TomtomService.new(customer: customer).get_vehicles_pos.each do |item|
           vehicle_id = item.delete :tomtom_vehicle_id
           vehicle = vehicles.detect{|v| v.tomtom_id == vehicle_id }
           next if !vehicle

app/services/orange_service.rb
@@ -16,17 +16,19 @@
 # <http://www.gnu.org/licenses/agpl.html>
 #
 class OrangeService < DeviceService
-  delegate :test_list, to: :service
+  def test_list params
+    service.test_list customer, params
+  end
 
   def list_devices
     with_cache "%s_%s" % [:list_devices, service_name, customer.id, customer.orange_user] do
-      service.list_devices
+      service.list_devices customer
     end
   end
 
   def get_vehicles_pos
     with_cache "%s_%s" % [:get_vehicles_pos, service_name, customer.id, customer.orange_user] do
-      service.get_vehicles_pos
+      service.get_vehicles_pos customer
     end
   end
 end

commit 61ce4916a7f1add79b96218b1288a7be08b3bfac

Devices:    * Fix Error Introduced In 81bb85818db30842909d69f3d14383de633f3df6      That Prevented TomTom Send / Clear    * Don't Pass Unused Options Parameter To Clear Method

app/services/device_service.rb
@@ -31,8 +31,8 @@ class DeviceService
     return route.last_sent_at
   end
 
-  def clear_route route, options={}
-    service.clear_route customer, route, options
+  def clear_route route
+    service.clear_route customer, route
     route.update! last_sent_at: nil
   end
 

lib/devices/device_helpers.rb
@@ -16,7 +16,7 @@ module Devices
 
     def device_clear_route options={}
       route = Route.for_customer(@customer).find params[:route_id]
-      service.clear_route(route, options)
+      service.clear_route route
       present route, with: V01::Entities::DeviceRouteLastSentAt
     end
 
@@ -24,7 +24,7 @@ module Devices
       planning = @customer.plannings.find params[:planning_id]
       routes = planning.routes.select(&:vehicle_usage)
       routes = routes.select{|route| route.vehicle_usage.vehicle.send(options[:device_id]) } if options[:device_id]
-      routes.each{|route| service.clear_route(route, options) }
+      routes.each{|route| service.clear_route(route) }
       present routes, with: V01::Entities::DeviceRouteLastSentAt
     end
 

lib/devices/orange.rb
@@ -43,7 +43,7 @@ class Orange < DeviceBase
     send_request send_xml_file(customer, route)
   end
 
-  def clear_route customer, route, options={}
+  def clear_route customer, route
     # Not supported by Garmin 590 -- Garmin Dezl and Nuvi only
     send_request send_xml_file(customer, route, { delete: true })
   end

lib/devices/teksat.rb
@@ -47,7 +47,7 @@ class Teksat < DeviceBase
     send_mission customer, route, route.end, route.vehicle_usage.default_store_stop.lat, route.vehicle_usage.default_store_stop.lng
   end
 
-  def clear_route customer, route, options={}
+  def clear_route customer, route
     response = RestClient.get get_missions_url(customer, { date: planning_date(route).strftime("%Y-%m-%d") })
     Nokogiri::XML(response).xpath("//mission").map{|item| RestClient.get(delete_mission_url(customer, { mi_id: item["id"] })) }
   end

lib/devices/tomtom.rb
@@ -59,8 +59,8 @@ class Tomtom < DeviceBase
     list_devices customer, { auth: params.slice(:account, :user, :password) }
   end
 
-  def list_devices customer, options={}
-    objects = get customer, savon_client_objects, :show_object_report, options
+  def list_devices customer, params={}
+    objects = get customer, savon_client_objects, :show_object_report, {}, params
     objects = [objects] if objects.is_a?(Hash)
     objects.select{ |object| !object[:deleted] }.collect do |object|
       {
@@ -188,8 +188,8 @@ class Tomtom < DeviceBase
     end
   end
 
-  def clear_route customer, route, options={}
-    get customer, savon_client_orders, :clear_orders, options, {
+  def clear_route customer, route
+    get customer, savon_client_orders, :clear_orders, {
       deviceToClear: {
         markDeleted: 'true',
       },
@@ -203,7 +203,7 @@ class Tomtom < DeviceBase
 
   private
 
-  def get customer, client, operation, options={}, message={}
+  def get customer, client, operation, message={}, options={}
     if options[:auth]
       account, username, password = options[:auth][:account], options[:auth][:user], options[:auth][:password]
     else