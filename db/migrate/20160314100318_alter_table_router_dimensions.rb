f8955f7f86134d7cc285646e26b15da9a8953e9a
Nicolas Gonzalez <nicolas@mapotempo.com>
2016-03-22 17:50:11 +0100

    RouterWrapper: Fix Migration

--- db/migrate/20160314100318_alter_table_router_dimensions.rb
+++ db/migrate/20160314100318_alter_table_router_dimensions.rb
@@ -1,11 +1,10 @@
+class RouterWrapperPublicTransport < ActiveRecord::Base ; end
+
 class AlterTableRouterDimensions < ActiveRecord::Migration
   def up
     add_column :routers, :time, :boolean, null: false, default: true
     add_column :routers, :distance, :boolean, null: false, default: false
-
-    Router.where(type: 'RouterWrapperPublicTransport').each{ |router|
-      router.type = 'RouterWrapper'
-    }
+    Router.where(type: 'RouterWrapperPublicTransport').update_all type: "RouterWrapper"
   end
 
   def down
