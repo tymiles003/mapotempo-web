d1470ab90b591a9d73d4a385ed2b85f6b12d4c65
Fabien Girard <fabien@mapotempo.com>
2016-04-12 11:01:50 +0200

    Add missing schema.rb for alyacom_api_key

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401092143) do
+ActiveRecord::Schema.define(version: 20160406140606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20160401092143) do
     t.string   "orange_user"
     t.string   "orange_password"
     t.integer  "router_dimension",                            default: 0,     null: false
+    t.string   "alyacom_api_key"
   end
 
   add_index "customers", ["job_destination_geocoding_id"], name: "index_customers_on_job_destination_geocoding_id", using: :btree
