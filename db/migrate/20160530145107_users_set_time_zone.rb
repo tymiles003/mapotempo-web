f46ecd10f3d5769e3d320cc7027455d41d112db6
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2016-06-08 18:46:09 +0200

    Fix time_zone migration

--- db/migrate/20160530145107_users_set_time_zone.rb
+++ db/migrate/20160530145107_users_set_time_zone.rb
@@ -1,10 +1,10 @@
 class UsersSetTimeZone < ActiveRecord::Migration
   def self.up
-    change_column :users, :time_zone, :string, null: false, default: "UTC"
-    User.update_all time_zone: "Europe/Paris"
+    change_column :users, :time_zone, :string, null: false, default: 'UTC'
+    User.update_all time_zone: 'Paris'
   end
   def self.down
     change_column :users, :time_zone, :string
-    User.update_all time_zone: "UTC"
+    User.update_all time_zone: 'UTC'
   end
 end
