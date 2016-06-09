421d058a6d84475651a016bea721be79968d11b9
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2016-06-09 18:12:22 +0200

    View: readd api_key in ical link

--- app/helpers/icalendar_url_helper.rb
+++ app/helpers/icalendar_url_helper.rb
@@ -2,15 +2,17 @@ require "erb"
 include ERB::Util
 
 module IcalendarUrlHelper
-  def api_planning_calendar_path user, planning
-    "/api/0.1/plannings/%s.ics" % [
-      planning.ref ? url_encode("ref:#{planning.ref}") : planning.id
+  def api_planning_calendar_path user, planning, query = nil
+    "/api/0.1/plannings/%s.ics%s" % [
+      planning.ref ? url_encode("ref:#{planning.ref}") : planning.id,
+      query ? "?" + query : ""
     ]
   end
-  def api_route_calendar_path user, route
-    "/api/0.1/plannings/%s/routes/%s.ics" % [
+  def api_route_calendar_path user, route, query = nil
+    "/api/0.1/plannings/%s/routes/%s.ics%s" % [
       route.planning.ref ? url_encode("ref:#{route.planning.ref}") : route.planning.id,
-      route.ref ? url_encode("ref:#{route.ref}") : route.id
+      route.ref ? url_encode("ref:#{route.ref}") : route.id,
+      query ? "?" + query : ""
     ]
   end
 end
