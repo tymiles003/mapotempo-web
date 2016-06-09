4fd138086f03d5d6811c06ba79bdc1d380147fff
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2016-06-09 17:14:47 +0200

    View: encode ical url

--- app/helpers/icalendar_url_helper.rb
+++ app/helpers/icalendar_url_helper.rb
@@ -1,13 +1,16 @@
+require "erb"
+include ERB::Util
+
 module IcalendarUrlHelper
   def api_planning_calendar_path user, planning
     "/api/0.1/plannings/%s.ics" % [
-      planning.ref ? "ref:#{planning.ref}" : planning.id
+      planning.ref ? url_encode("ref:#{planning.ref}") : planning.id
     ]
   end
   def api_route_calendar_path user, route
     "/api/0.1/plannings/%s/routes/%s.ics" % [
-      route.planning.ref ? "ref:#{route.planning.ref}" : route.planning.id,
-      route.ref ? "ref:#{route.ref}" : route.id
+      route.planning.ref ? url_encode("ref:#{route.planning.ref}") : route.planning.id,
+      route.ref ? url_encode("ref:#{route.ref}") : route.id
     ]
   end
 end
