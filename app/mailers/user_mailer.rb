bd6bac36070cb3aeb29ec18aed49965ea48b04c2
Fabien Girard <fabien@mapotempo.com>
2016-06-09 12:48:57 +0200

    Fix send welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def welcome_message user
-    mail to: user.email, subject: t("user_mailer.welcome.title") do |format|
+    mail to: user.email, subject: t("user_mailer.welcome.title", s: user.customer.reseller.name) do |format|
       format.html { render 'user_mailer/welcome', locals: { user: user } }
     end
   end
