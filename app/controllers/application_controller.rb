1482f20c30489c3efea48b0e23f202c77e104364
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2016-06-09 12:09:38 +0200

    Ensure locale is set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -54,7 +54,7 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.locale = http_accept_language.compatible_language_from %w(en fr)
+    I18n.locale = http_accept_language.compatible_language_from(%w(en fr)) || I18n.default_locale
   end
 
   def devise_parameter_sanitizer
