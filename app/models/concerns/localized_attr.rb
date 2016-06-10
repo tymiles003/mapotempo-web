ea3c114407a09ed0362c1258661e665e75c88a38
Fabien Girard <fabien@mapotempo.com>
2016-06-10 13:57:41 +0200

    Fix localized numbers

--- app/models/concerns/localized_attr.rb
+++ app/models/concerns/localized_attr.rb
@@ -14,6 +14,6 @@ module LocalizedAttr
   def to_delocalized_decimal value
     delimiter = I18n::t('number.format.delimiter')
     separator = I18n::t('number.format.separator')
-    value.gsub /[#{delimiter}#{separator}]/, delimiter => '', separator => '.'
+    value.gsub("#{separator}", '.').gsub(/#{delimiter}([0-9]{3})/, '\1')
   end
 end
