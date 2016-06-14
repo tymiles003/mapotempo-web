1fa3dc58c6c1380be250f4e2cd9c03e983c47277
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2016-06-14 15:29:32 +0200

    Mailers: rubocop

--- app/mailers/route_mailer.rb
+++ app/mailers/route_mailer.rb
@@ -1,5 +1,5 @@
 class RouteMailer < ApplicationMailer
-  def send_kmz_route user, locale, vehicle, route, filename, attachment
+  def send_kmz_route(user, locale, vehicle, route, filename, attachment)
     I18n.with_locale(locale) do
       attachments[filename] = { transfer_encoding: :binary, content: attachment.force_encoding('BINARY') }
       mail from: user.email, to: vehicle.contact_email, subject: "[#{user.customer.reseller.name}] #{filename}" do |format|
@@ -7,7 +7,7 @@ class RouteMailer < ApplicationMailer
       end
     end
   end
-  def send_ics_route user, locale, vehicle, route, filename, url
+  def send_ics_route(user, locale, vehicle, route, filename, url)
     I18n.with_locale(locale) do
       mail from: user.email, to: vehicle.contact_email, subject: "[#{user.customer.reseller.name}] #{filename}" do |format|
         format.text { render 'route_mailer/send_ics_route', locals: { user: user, vehicle: vehicle, route: route, url: url } }
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  def welcome_message user, locale
+  def welcome_message(user, locale)
     I18n.with_locale(locale) do
       mail to: user.email, subject: t("user_mailer.welcome.title", s: user.customer.reseller.name) do |format|
         format.html { render 'user_mailer/welcome', locals: { user: user } }
