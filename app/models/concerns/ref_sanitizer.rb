d74313ce9253548fef6963f543a2669e13531eeb
Nicolas Gonzalez <nicolas@mapotempo.com>
2016-07-01 17:25:55 +0200

    Ref Sanitizer: Strip Whitespaces

--- app/models/concerns/ref_sanitizer.rb
+++ app/models/concerns/ref_sanitizer.rb
@@ -2,12 +2,10 @@ module RefSanitizer
   extend ActiveSupport::Concern
 
   included do
-    validate do |record|
-      if !record.ref.blank?
-        record.ref.strip!
-        if record.ref =~ %r{[\./\\]}
-          record.errors[:ref] << I18n.t('activerecord.errors.models.planning.attributes.ref.invalid_format')
-        end
+    define_method "ref=" do |value|
+      self[:ref] = value.strip if value
+      if self.ref =~ /[\.\/\\]/
+        record.errors[:ref] << I18n.t('activerecord.errors.models.planning.attributes.ref.invalid_format')
       end
     end
   end
