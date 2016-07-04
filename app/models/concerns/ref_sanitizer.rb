1297e490d4c5202a4f3b21dd141c9e3eb223da50
Nicolas Gonzalez <nicolas@mapotempo.com>
2016-07-04 10:48:46 +0200

    Revert "Ref Sanitizer: Strip Whitespaces"
    This reverts commit d74313ce9253548fef6963f543a2669e13531eeb.

--- app/models/concerns/ref_sanitizer.rb
+++ app/models/concerns/ref_sanitizer.rb
@@ -2,10 +2,12 @@ module RefSanitizer
   extend ActiveSupport::Concern
 
   included do
-    define_method "ref=" do |value|
-      self[:ref] = value.strip if value
-      if self.ref =~ /[\.\/\\]/
-        record.errors[:ref] << I18n.t('activerecord.errors.models.planning.attributes.ref.invalid_format')
+    validate do |record|
+      if !record.ref.blank?
+        record.ref.strip!
+        if record.ref =~ %r{[\./\\]}
+          record.errors[:ref] << I18n.t('activerecord.errors.models.planning.attributes.ref.invalid_format')
+        end
       end
     end
   end
