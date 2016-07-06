10e8e053d2a3650f43915334b5731b341c8b15cf
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2016-07-06 12:06:15 +0200

    Update optim wrapper schema

--- lib/optim/optimizer_wrapper.rb
+++ lib/optim/optimizer_wrapper.rb
@@ -80,11 +80,15 @@ class OptimizerWrapper
           cost_late_multiplier: dimension == 'time' ? soft_upper_bound : 0,
           rests: rests.each_with_index.collect{ |rest, index| "r#{index + services.size + 1 + 1}" }
         }],
-        resolution: {
-          preprocessing_cluster_threshold: cluster_threshold,
-          preprocessing_prefer_short_segment: true,
-          duration: optimize_time,
-         }
+        configuration: {
+          preprocessing: {
+            cluster_threshold: cluster_threshold,
+            prefer_short_segment: true
+          },
+          resolution: {
+            duration: optimize_time
+          }
+        }
       }
 
       resource_vrp = RestClient::Resource.new(@url + '/vrp/submit.json')
