ac8c93f5b19d9747ae7eaca259acc4cbd16978b7
Nicolas Gonzalez <nicolas@mapotempo.com>
2016-07-18 11:06:24 +0200

    Plannings: Fix Zonings Plus Button

--- app/helpers/zonings_helper.rb
+++ app/helpers/zonings_helper.rb
@@ -35,7 +35,7 @@ module ZoningsHelper
   end
 
   def zoning_select(f, planning, label=false)
-    input_group = content_tag('a', content_tag('i', nil, class: 'fa fa-plus fa-fw'), href: [:new, :zonings, planning, back: true], class: 'btn btn-default', title: t('plannings.edit.zoning_new')) if !planning.new_record?
+    input_group = content_tag('a', content_tag('i', nil, class: 'fa fa-plus fa-fw'), href: url_for([:new, :zonings, planning, back: true]), class: 'btn btn-default', title: t('plannings.edit.zoning_new')) if !planning.new_record?
     f.select :zoning_ids, label, options_for_select(planning.customer.zonings.map{ |zoning| [zoning.name, zoning.id] }, planning.zonings.map(&:id)), {}, { multiple: true, input_group: input_group }
   end
 end
