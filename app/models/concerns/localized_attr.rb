8e30f79992a34e9e0b1da84ee6a9ca8920128365
Nicolas Gonzalez <nicolas@mapotempo.com>
2016-07-20 17:15:45 +0200

    Models: Localize Numeric Values

--- app/models/concerns/localized_attr.rb
+++ app/models/concerns/localized_attr.rb
@@ -1,11 +1,15 @@
 module LocalizedAttr
   extend ActiveSupport::Concern
+  include ActionView::Helpers::NumberHelper
 
   class_methods do
     def attr_localized(*fields)
       fields.each do |field|
         define_method("#{field}=") do |value|
           self[field] = value.is_a?(String) ? to_delocalized_decimal(value) : value
+        end
+        define_method("localized_#{field}") do
+          number_with_delimiter(send(field).to_s.gsub('.', I18n.t('number.format.separator')))
         end
       end
     end
