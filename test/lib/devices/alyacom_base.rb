dbe105d6baf1cb222682264107b25272f3a02230
Nicolas Gonzalez <nicolas@mapotempo.com>
2016-07-29 10:06:19 +0200

    Alyacom: Update Tests

--- test/lib/devices/alyacom_base.rb
+++ test/lib/devices/alyacom_base.rb
@@ -1,5 +1,7 @@
 module AlyacomBase
 
+  require 'erb'
+
   def set_route
     @route = routes(:route_one_one)
     @route.update! end: @route.start + 5.hours
@@ -22,7 +24,7 @@ module AlyacomBase
             names.each do |name|
               case name
                 when :staff, :users, :planning
-                  expected_response = File.read(Rails.root.join("test/web_mocks/alyacom.fr/#{name}.json")).strip
+                  expected_response = ERB.new(File.read(Rails.root.join("test/web_mocks/alyacom.fr/#{name}.json.erb"))).result(binding)
                   url = [URI.parse(Mapotempo::Application.config.devices.alyacom.api_url), @customer.alyacom_association, name.to_s].join "/"
                   stubs << stub_request(method, url).with(query: hash_including({ })).to_return(status: 200, body: expected_response)
               end
