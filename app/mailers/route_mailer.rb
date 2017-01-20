49b2ef8847a5b71f9ede999e2847d4ba40d1d179
Nicolas Perussel <nicolas.perussel@mapotempo.com>
2017-01-20 12:33:15 +0100

    Remove from: to prevent spam in export ical

--- app/mailers/route_mailer.rb
+++ app/mailers/route_mailer.rb
@@ -3,7 +3,7 @@ class RouteMailer < ApplicationMailer
   def send_kmz_route(user, locale, vehicle, route, filename, attachment)
     I18n.with_locale(locale) do
       attachments[filename] = { transfer_encoding: :binary, content: attachment.force_encoding('BINARY') }
-      mail from: user.email, to: vehicle.contact_email, subject: "[#{user.customer.reseller.name}] #{filename}" do |format|
+      mail to: vehicle.contact_email, subject: "[#{user.customer.reseller.name}] #{filename}" do |format|
         format.text { render 'route_mailer/send_kmz_route', locals: { vehicle: vehicle, route: route } }
       end
     end
@@ -11,7 +11,7 @@ class RouteMailer < ApplicationMailer
   
   def send_computed_ics_route(user, locale, email, vehicles)
     I18n.with_locale(locale) do
-      mail from: user.email, to: email, subject: "[#{user.customer.reseller.name}] Export Planning iCalendar" do |format|
+      mail to: email, subject: "[#{user.customer.reseller.name}] Export Planning iCalendar" do |format|
         format.text { render 'route_mailer/send_computed_ics_route', locals: { user: user, infos: vehicles } }
       end
     end
