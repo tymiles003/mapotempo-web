99bc09d54597d9f7fdf40f813457fc3f800d0a05
FloMapo <florian.paviot@mapotempo.com>
2017-03-15 11:21:30 +0100

    Correct error when removing a disabled vehicle

--- app/models/planning.rb
+++ app/models/planning.rb
@@ -96,10 +96,13 @@ class Planning < ActiveRecord::Base
 
   def vehicle_usage_remove(vehicle_usage)
     route = routes.find{ |route| route.vehicle_usage == vehicle_usage}
-    route.stops.select{ |stop| stop.is_a?(StopVisit) }.collect{ |stop|
-      routes.find{ |r| !r.vehicle_usage }.stops.build(type: StopVisit.name, visit: stop.visit)
-    }
-    routes.destroy(route)
+    # route no longer exists if vehicle is disabled
+    if route
+      route.stops.select{ |stop| stop.is_a?(StopVisit) }.collect{ |stop|
+        routes.find{ |r| !r.vehicle_usage }.stops.build(type: StopVisit.name, visit: stop.visit)
+      }
+      routes.destroy(route)
+    end
   end
 
   def visit_add(visit)
