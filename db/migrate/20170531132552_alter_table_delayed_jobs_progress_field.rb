1793b6e221d84ddd74eb816addf65be85319a2eb
FloMapo <florian.paviot@mapotempo.com>
2017-07-24 17:24:46 +0200

    Change migration for progress field of delayed jobs table

--- db/migrate/20170531132552_alter_table_delayed_jobs_progress_field.rb
+++ db/migrate/20170531132552_alter_table_delayed_jobs_progress_field.rb
@@ -2,10 +2,21 @@ class AlterTableDelayedJobsProgressField < ActiveRecord::Migration
   def up
     change_column_null :delayed_jobs, :progress, true
     change_column_default :delayed_jobs, :progress, nil
+
+    Delayed::Backend::ActiveRecord::Job.all.each do |delayed_job|
+      delayed_job.progress = nil if delayed_job.progress == '0'
+      delayed_job.save!
+    end
   end
 
   def down
     change_column_default :delayed_jobs, :progress, '0'
+
+    Delayed::Backend::ActiveRecord::Job.all.each do |delayed_job|
+      delayed_job.progress = '0' unless delayed_job.progress
+      delayed_job.save!
+    end
+
     change_column_null :delayed_jobs, :progress, false
   end
 end
