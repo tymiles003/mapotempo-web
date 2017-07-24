f0d35b362b1025bead557402dc8b1db64c4a5ecb
FloMapo <florian.paviot@mapotempo.com>
2017-07-24 15:31:39 +0200

    Desactivate redirection after user connection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,6 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
 
   # saves the location before loading each page so we can return to the right page.
-  before_action :store_current_location, unless: :devise_controller?
   before_action :api_key?, :load_vehicles
   before_action :set_locale
   before_action :customer_payment_period, if: :current_user
@@ -123,23 +122,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  private
-
-  def store_current_location
-    store_location_for(:user, request.url) if request.get?
-  end
-
-  def after_sign_out_path_for(_resource)
-    request.referrer || root_path
-  end
-
-  private
-
-  def store_current_location
-    store_location_for(:user, request.url) if request.get?
-  end
-
-  def after_sign_out_path_for(_resource)
-    request.referrer || root_path
-  end
 end
