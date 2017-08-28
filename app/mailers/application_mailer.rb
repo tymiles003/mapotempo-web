de503cc4c1a083e034825487f62266c28f6dbc0c
FloMapo <florian.paviot@mapotempo.com>
2017-08-28 15:59:37 +0200

    Display images in emails with correct form

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
+  add_template_helper(EmailHelper)
+
   default from: Mapotempo::Application.config.default_from_mail
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,12 +3,13 @@ class UserMailer < ApplicationMailer
     I18n.with_locale(locale) do
       @name = user.customer.reseller.name
       @email = user.email
+      @test = user.customer.test
 
       @title = t('user_mailer.welcome.title')
       @confirmation_link = user.customer.reseller.url_protocol + "://" + user.customer.reseller.host + password_user_path(user, token: user.confirmation_token)
       @home_link = user.customer.reseller.url_protocol + "://" + user.customer.reseller.host
       @contact_link = (user.customer.reseller.contact_url && user.customer.reseller.contact_url.gsub('{LG}', I18n.locale.to_s))
-      @subscription_duration = user.customer.end_subscription && (user.customer.end_subscription - Date.today).to_i > 1 ? (user.customer.end_subscription - Date.today).to_i : 15
+      @subscription_duration = user.customer.end_subscription && (user.customer.end_subscription - Date.today).to_i > 1 ? (user.customer.end_subscription - Date.today).to_i : nil
 
       mail to: @email, subject: t('user_mailer.welcome.subject', name: @name) do |format|
         format.html { render 'user_mailer/welcome', locals: { user: user } }
