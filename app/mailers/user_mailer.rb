3b87abb6dd7619c41afb28428060fe7ec697c399
FloMapo <florian.paviot@mapotempo.com>
2017-09-07 16:52:13 +0200

    Factorize email template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,12 +6,14 @@ class UserMailer < ApplicationMailer
       @test = user.customer.test
 
       @title = t('user_mailer.welcome.title')
+      @logo_link = user.customer.reseller.logo_large.url || 'logo_mapotempo.png'
       @confirmation_link = user.customer.reseller.url_protocol + "://" + user.customer.reseller.host + password_user_path(user, token: user.confirmation_token)
       @home_link = user.customer.reseller.url_protocol + "://" + user.customer.reseller.host
+      @help_link = user.customer.reseller.help_url if user.customer.reseller.help_url.present?
       @facebook_link = user.customer.reseller.facebook_url if user.customer.reseller.facebook_url.present?
       @twitter_link = user.customer.reseller.twitter_url if user.customer.reseller.twitter_url.present?
       @linkedin_link = user.customer.reseller.linkedin_url if user.customer.reseller.linkedin_url.present?
-      @contact_link = (user.customer.reseller.contact_url && user.customer.reseller.contact_url.gsub('{LG}', I18n.locale.to_s))
+      @contact_link = user.customer.reseller.contact_url.gsub('{LG}', I18n.locale.to_s) if user.customer.reseller.contact_url
       @subscription_duration = user.customer.end_subscription && (user.customer.end_subscription - Date.today).to_i > 1 ? (user.customer.end_subscription - Date.today).to_i : nil
 
       mail to: @email, subject: t('user_mailer.welcome.subject', name: @name) do |format|
