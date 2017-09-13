ac59f7e6e5128237e151f3c6bcb98f325d7ae21a
FloMapo <florian.paviot@mapotempo.com>
2017-09-13 12:05:36 +0200

    Improve KMZ and ICS emails

--- app/mailers/route_mailer.rb
+++ app/mailers/route_mailer.rb
@@ -2,12 +2,13 @@ class RouteMailer < ApplicationMailer
 
   def send_kmz_route(user, locale, vehicle, route, filename, attachment)
     I18n.with_locale(locale) do
+      @name = user.customer.reseller.name
       @logo_link = user.customer.reseller.logo_large.url || 'logo_mapotempo.png'
       @facebook_link = user.customer.reseller.facebook_url if user.customer.reseller.facebook_url.present?
       @twitter_link = user.customer.reseller.twitter_url if user.customer.reseller.twitter_url.present?
       @linkedin_link = user.customer.reseller.linkedin_url if user.customer.reseller.linkedin_url.present?
 
-      @title = filename
+      @title = I18n.t('route_mailer.send_kmz_route.title')
       attachments[filename] = { transfer_encoding: :binary, content: attachment.force_encoding('BINARY') }
       mail to: vehicle.contact_email, subject: "[#{user.customer.reseller.name}] #{filename}" do |format|
         format.html { render 'route_mailer/send_kmz_route', locals: { vehicle: vehicle, route: route } }
@@ -17,6 +18,7 @@ class RouteMailer < ApplicationMailer
 
   def send_computed_ics_route(user, locale, email, vehicles)
     I18n.with_locale(locale) do
+      @name = user.customer.reseller.name
       @logo_link = user.customer.reseller.logo_large.url || 'logo_mapotempo.png'
       @facebook_link = user.customer.reseller.facebook_url if user.customer.reseller.facebook_url.present?
       @twitter_link = user.customer.reseller.twitter_url if user.customer.reseller.twitter_url.present?
