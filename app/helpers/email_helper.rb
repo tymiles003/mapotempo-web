2a1e2553cef618cb6d0c7339748b94605d6caf51
Julien Boyer <julien.boyer@mapotempo.com>
2017-10-09 14:29:56 +0200

    Automation Fix | Don't attach same image link multiple time

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -3,9 +3,9 @@ module EmailHelper
     image_name = image
     if image.start_with?('/uploads')
       image_name = image.split('/').last
-      attachments[image_name] = File.read(Rails.root.join("public/#{image}"))
+      attachments[image_name] = File.read(Rails.root.join("public/#{image}")) if attachments[image_name].nil?
     else
-      attachments[image_name] = File.read(Rails.root.join("app/assets/images/#{image}"))
+      attachments[image_name] = File.read(Rails.root.join("app/assets/images/#{image}")) if attachments[image_name].nil?
     end
 
     image_tag attachments[image_name].url, **options
