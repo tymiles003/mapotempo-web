e9b6ae396bc537a3b8aca23faf0c31371f2a200c
FloMapo <florian.paviot@mapotempo.com>
2017-10-09 11:46:23 +0200

    Correct link path for uploaded files in emails

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,6 +1,13 @@
 module EmailHelper
   def email_image_tag(image, **options)
-    attachments[image] = File.read(Rails.root.join("app/assets/images/#{image}"))
-    image_tag attachments[image].url, **options
+    image_name = image
+    if image.start_with?('/uploads')
+      image_name = image.split('/').last
+      attachments[image_name] = File.read(Rails.root.join("public/#{image}"))
+    else
+      attachments[image_name] = File.read(Rails.root.join("app/assets/images/#{image}"))
+    end
+
+    image_tag attachments[image_name].url, **options
   end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -15,6 +15,24 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal I18n.t('user_mailer.password.subject', name: reseller_name), email.subject
   end
 
+  test 'should send password email with uploaded image' do
+    user_one = users(:user_one)
+    reseller = user_one.customer.reseller
+    reseller_name = user_one.customer.reseller.name
+    file = Rack::Test::UploadedFile.new(Rails.root.join('test', 'fixtures', 'logo', 'logo_mapotempo.png'), 'image/png')
+    reseller.logo_large = file
+    reseller.save!
+
+    email = UserMailer.password_message(user_one, I18n.locale)
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['u1@plop.com'], email.to
+    assert_equal I18n.t('user_mailer.password.subject', name: reseller_name), email.subject
+  end
+
   test 'should send connection email' do
     user_one = users(:user_one)
     reseller_name = user_one.customer.reseller.name
