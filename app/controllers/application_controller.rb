0a059dcdde173c8e42fb1aa8f6d0271525c5ab10
Fabien Girard <fabien@mapotempo.com>
2017-11-16 15:15:40 +0100

    Keep traces in tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # Handle exceptions
-  rescue_from StandardError, with: :server_error if ENV['RAILS_ENV'] != 'development'
+  rescue_from StandardError, with: :server_error if ENV['RAILS_ENV'] == 'production'
   rescue_from ActionController::InvalidAuthenticityToken, with: :server_error
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_error
   rescue_from AbstractController::ActionNotFound, with: :not_found_error
